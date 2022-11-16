7b8bc3a2ad7dfe6a0a1360c48f7fecbf48f21735
darleeenman <darleen@kindxd.com>
2022-11-15 18:21:52 -0800

    update forecast
    JS forecast + html

--- function.js
+++ function.js
@@ -28,6 +28,33 @@ function formatDate(date) {
   return `${hours}:${minutes}`;
 }
 
+// forecast
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Wed", "Thurs", "Fri", "Sat", "Sun", "Mon"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+      <div class="weather-forecast-date">${day}</div>
+        <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="42" />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+          </div>
+        </div>
+    `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 // search
 function search(event) {
   event.preventDefault();
@@ -46,17 +73,6 @@ let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
 let temperature;
-
-// conversion C - F
-// function convertToFahrenheit(event) {
-//   event.preventDefault();
-//   let temperatureElement = document.querySelector("#temperature");
-// }
-
-// function convertToCelsius(event) {
-//   event.preventDefault();
-//   let temperatureElement = document.querySelector("#temperature");
-// }
 
 // current button
 let currentButton = document.querySelector("#current-button");
@@ -147,3 +163,5 @@ function displayCelsiusTemperature(event) {
   let temperatureElement = document.querySelector("#current-temp");
   temperatureElement.innerHTML = Math.round(temperature);
 }
+
+displayForecast();
